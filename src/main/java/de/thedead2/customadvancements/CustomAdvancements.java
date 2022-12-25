2b4d7612b1212e66d2925bfb194551ef6c18e22a
The_Dead_2 <thedead2@users.noreply.github.com>
2022-12-26 00:01:33 +0100

    adding Update Checker

--- src/main/java/de/thedead2/customadvancements/CustomAdvancements.java
+++ src/main/java/de/thedead2/customadvancements/CustomAdvancements.java
@@ -1,8 +1,10 @@
 package de.thedead2.customadvancements;
 
+import de.thedead2.customadvancements.util.VersionControl;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,16 +14,23 @@ import static de.thedead2.customadvancements.util.ModHelper.*;
 @Mod(MOD_ID)
 public class CustomAdvancements {
     public static final Logger LOGGER = LogManager.getLogger();
+    private static final VersionControl versionControl = new VersionControl();
 
     public CustomAdvancements() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onServerStarted);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("Starting " + MOD_NAME + ", Version: " + MOD_VERSION);
+        versionControl.loggerMessage();
 
         FILE_HANDLER.getDirectory();
         FILE_HANDLER.readFiles();
+    }
+
+    private void onServerStarted(final FMLServerStartedEvent event){
+        VersionControl.chatMessage(event.getServer());
     }
 }
--- src/main/java/de/thedead2/customadvancements/util/ModHelper.java
+++ src/main/java/de/thedead2/customadvancements/util/ModHelper.java
@@ -1,15 +1,18 @@
 package de.thedead2.customadvancements.util;
 
+import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.fml.loading.moddiscovery.ModFile;
 
 public abstract class ModHelper {
 
-    public static final String MOD_VERSION = "1.16.5-1.0.1.0";
+    public static final String MOD_VERSION = "1.16.5-1.0.1";
     public static final String MOD_ID = "customadvancements";
     public static final String MOD_NAME = "Custom Advancements";
+    public static final String MOD_UPDATE_LINK = "https://www.curseforge.com/minecraft/mc-mods/custom-advancements/files";
     public static final ModFile THIS_MOD_FILE = ModList.get().getModFileById(MOD_ID).getFile();
+    public static final ModContainer THIS_MOD_CONTAINER = ModList.get().getModContainerById(MOD_ID).orElseThrow(RuntimeException::new);
 
     public static final String GAME_DIR = FMLPaths.GAMEDIR.get().toString();
     public static final String DIR_PATH = GAME_DIR + "/" + MOD_ID;
