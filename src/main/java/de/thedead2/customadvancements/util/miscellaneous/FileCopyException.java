61958f11f3239a1467de0515f8e2efbe4297e0ca
The_Dead_2 <62204843+thedead2@users.noreply.github.com>
2023-01-15 22:33:01 +0100

    Delete src/main directory

--- src/main/java/de/thedead2/customadvancements/util/miscellaneous/FileCopyException.java
+++
@@ -1,8 +0,0 @@
-package de.thedead2.customadvancements.util.miscellaneous;
-
-public class FileCopyException extends RuntimeException {
-
-    public FileCopyException(String message){
-        super(message);
-    }
-}
