31fe6b96999bb1d01d47e373fe029e4c66708749
The_Dead_2 <thedead2@users.noreply.github.com>
2023-01-30 10:04:33 +0100

    converted to current release version

--- src/main/java/de/thedead2/customadvancements/commands/ModGameRules.java
+++
@@ -1,8 +0,0 @@
-package de.thedead2.customadvancements.commands;
-
-import net.minecraft.world.level.GameRules;
-
-public class ModGameRules {
-
-    public static final GameRules.Key<GameRules.BooleanValue> ADVANCEMENT_GENERATOR_GAMERULE = GameRules.register("enableAdvancementGenerator", GameRules.Category.MISC, GameRules.BooleanValue.create(false));
-}
