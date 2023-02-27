8dfa8b381fb7c1f816a5947027c53166935bccd7
The_Dead_2 <thedead2@users.noreply.github.com>
2023-02-27 23:03:02 +0100

    CrashHandler implementation

---
+++ src/main/java/de/thedead2/customadvancements/mixin/MixinCrashReport.java
@@ -0,0 +1,53 @@
+package de.thedead2.customadvancements.mixin;
+
+import de.thedead2.customadvancements.util.ModHelper;
+import de.thedead2.customadvancements.util.handler.CrashHandler;
+import de.thedead2.customadvancements.util.logger.ConsoleColors;
+import net.minecraft.CrashReport;
+import net.minecraft.CrashReportCategory;
+import org.apache.logging.log4j.Level;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Mixin(CrashReport.class)
+public abstract class MixinCrashReport {
+
+    @Shadow @Final private Throwable exception;
+    @Shadow @Final private List<CrashReportCategory> details;
+
+    @Inject(at = @At("HEAD"), method = "getFriendlyReport")
+    public void onFriendlyReport(CallbackInfoReturnable<String> cir){
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        if(crashHandler.resolveCrash(exception)){
+            return;
+        }
+        details.forEach(crashReportCategory -> {
+            AtomicReference<String> errorMessage = new AtomicReference<>();
+            crashReportCategory.entries.forEach(crashReportCategory$Entry -> {
+                String key = crashReportCategory$Entry.getKey();
+                if(key.contains("Exception")){
+                    errorMessage.set(crashReportCategory$Entry.getValue());
+                    if(crashHandler.resolveCrash(crashReportCategory.getStacktrace(), errorMessage.get())){
+                        return;
+                    }
+                }
+                else if(key.contains("Screen")){
+                    if(crashReportCategory$Entry.getValue().contains("de.thedead2.customadvancements")){
+                        crashHandler.addCrashDetails("Error while rendering screen: " + crashReportCategory$Entry.getValue() +
+                                "\n\t\t\t\t" + ConsoleColors.italic + " Please note that this error was not caused by " + ModHelper.MOD_NAME + "! So don't report it to the mod author!" + ConsoleColors.reset,
+                                Level.FATAL, exception, true
+                        );
+                        return;
+                    }
+                }
+            });
+        });
+    }
+}
