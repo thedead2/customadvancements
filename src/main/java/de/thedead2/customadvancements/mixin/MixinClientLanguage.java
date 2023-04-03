c310d2cfd73134a76ccbfe9ba05ffdc8c9d2cc06
The_Dead_2 <thedead2@users.noreply.github.com>
2023-04-03 11:32:05 +0200

    LanguageHandler and ResourceManagerExtender

---
+++ src/main/java/de/thedead2/customadvancements/mixin/MixinClientLanguage.java
@@ -0,0 +1,21 @@
+package de.thedead2.customadvancements.mixin;
+
+import de.thedead2.customadvancements.util.handler.LanguageHandler;
+import net.minecraft.client.resources.language.ClientLanguage;
+import net.minecraft.server.packs.resources.Resource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Map;
+
+@Mixin(ClientLanguage.class)
+public class MixinClientLanguage {
+
+    @Inject(at = @At("TAIL"), method = "appendFrom(Ljava/lang/String;Ljava/util/List;Ljava/util/Map;)V")
+    private static void onLoad(String pLanguageName, List<Resource> pResources, Map<String, String> pDestinationMap, CallbackInfo ci){
+        LanguageHandler.getInstance().inject(pLanguageName, pDestinationMap);
+    }
+}
