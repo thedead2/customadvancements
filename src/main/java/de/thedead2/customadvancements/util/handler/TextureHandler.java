5408db6d5833eff3a0f2447a8a8c73fe73c501b0
The_Dead_2 <thedead2@users.noreply.github.com>
2023-09-22 12:55:06 +0200

    Extended support for different image types...

--- src/main/java/de/thedead2/customadvancements/util/handler/TextureHandler.java
+++ src/main/java/de/thedead2/customadvancements/util/handler/TextureHandler.java
@@ -6,10 +6,13 @@ import de.thedead2.customadvancements.util.core.FileHandler;
 import net.minecraft.resources.ResourceLocation;
 
 import java.io.File;
+import java.util.Arrays;
 
 import static de.thedead2.customadvancements.util.core.ModHelper.*;
 
 public abstract class TextureHandler {
+    private static final String[] valid_file_extensions = {".png", ".jpeg", ".jpg", ".hdr", ".bmp", ".tga", ".psd", ".gif", ".pic", ".pnm"};
+
     public static void start() {
         FileHandler.readDirectory(TEXTURES_PATH.toFile(), directory -> {
             File[] texture_files = directory.listFiles();
@@ -19,13 +22,17 @@ public abstract class TextureHandler {
             assert texture_files != null;
             for (File texture : texture_files) {
                 CrashHandler.getInstance().setActiveFile(texture);
-                if (texture.getName().endsWith(".png")) {
-                    LOGGER.debug("Found file: " + texture.getName());
 
-                    ResourceManagerExtender.addResource(ResourceLocation.tryParse(MOD_ID + ":" + "textures" + "/" + texture.getName()), texture);
+                final String fileName = texture.getName();
+                final String fileExtension = fileName.substring(texture.getName().lastIndexOf('.'));
+
+                if (fileExtension.matches("(?i)" + String.join("|", valid_file_extensions))) {
+                    LOGGER.debug("Found file: " + fileName);
+
+                    ResourceManagerExtender.addResource(ResourceLocation.tryParse(MOD_ID + ":" + "textures" + "/" + fileName), texture);
                 }
                 else {
-                    LOGGER.warn("File '" + texture.getName() + "' is not a '.png' file, ignoring it!");
+                    LOGGER.warn("File '" + fileName + "' is not a valid texture file, ignoring it! --> supported file types: {}", Arrays.toString(valid_file_extensions));
                 }
             }
             CrashHandler.getInstance().setActiveFile(null);
