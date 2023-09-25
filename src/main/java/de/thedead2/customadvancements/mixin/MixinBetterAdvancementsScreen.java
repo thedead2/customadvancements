8cde5a37bc95aa2228f57c2fd69b65ea44361ba6
The_Dead_2 <thedead2@users.noreply.github.com>
2023-09-25 16:00:39 +0200

    Fixes #8 in production for 1.19.3...

--- src/main/java/de/thedead2/customadvancements/mixin/MixinBetterAdvancementsScreen.java
+++ src/main/java/de/thedead2/customadvancements/mixin/MixinBetterAdvancementsScreen.java
@@ -1,36 +1,24 @@
 package de.thedead2.customadvancements.mixin;
 
-import betteradvancements.gui.BetterAdvancementTab;
-import de.thedead2.customadvancements.util.core.ConfigManager;
+import betteradvancements.gui.BetterAdvancementsScreen;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementProgress;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Map;
 
 @Pseudo
-@Mixin(targets = "betteradvancements.gui.BetterAdvancementsScreen")
+@Mixin(BetterAdvancementsScreen.class)
 public class MixinBetterAdvancementsScreen {
 
-    @Shadow(remap = false) @Final private Map<Advancement, BetterAdvancementTab> tabs;
 
     @Inject(at = @At("HEAD"), method = "onUpdateAdvancementProgress", cancellable = true, remap = false)
     public void onUpdateAdvancementProgress(Advancement advancementIn, AdvancementProgress progress, CallbackInfo ci){
         if (advancementIn == null){
             ci.cancel();
         }
-    }
-
-    @Inject(at =  @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", shift = At.Shift.AFTER), method = "onAddAdvancementRoot(Lnet/minecraft/advancements/Advancement;)V", locals =
-            LocalCapture.CAPTURE_FAILSOFT, remap = false)
-    public void onNewAdvancementTab(Advancement pAdvancement, CallbackInfo ci){
-        ConfigManager.ADVANCEMENT_TAB_SORTING_MODE.get().sortAdvancementTabs(this.tabs);
     }
 }
