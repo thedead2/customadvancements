d38963431b09aa7d9a8766f65bde692d1de55dfa
The_Dead_2 <thedead2@users.noreply.github.com>
2023-09-25 16:00:52 +0200

    Fixes #8 in production for 1.19.3...

--- src/main/java/de/thedead2/customadvancements/client/ScreenHandler.java
+++ src/main/java/de/thedead2/customadvancements/client/ScreenHandler.java
@@ -1,7 +1,10 @@
-package de.thedead2.customadvancements.util.handler;
+package de.thedead2.customadvancements.client;
 
+import betteradvancements.gui.BetterAdvancementsScreen;
 import de.thedead2.customadvancements.util.core.ConfigManager;
+import de.thedead2.customadvancements.util.core.CrashHandler;
 import de.thedead2.customadvancements.util.core.ModHelper;
+import net.minecraft.advancements.Advancement;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.GridWidget;
@@ -15,13 +18,20 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.Level;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import static de.thedead2.customadvancements.util.core.ModHelper.BA_COMPATIBILITY;
+
 
 @Mod.EventBusSubscriber(modid = ModHelper.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ScreenHandler {
     @SubscribeEvent
+    @SuppressWarnings("unchecked")
     public static void afterScreenInit(ScreenEvent.Init.Post event) {
         Screen screen = event.getScreen();
         if(screen instanceof PauseScreen pauseScreen){
@@ -43,11 +53,24 @@ public class ScreenHandler {
                 });
             }
         }
+        else if(BA_COMPATIBILITY.get() && screen instanceof BetterAdvancementsScreen betterAdvancementsScreen){
+            try {
+                var clazz = betterAdvancementsScreen.getClass();
+                Field tabs = clazz.getDeclaredField("tabs");
+                tabs.setAccessible(true);
+                Object obj = tabs.get(betterAdvancementsScreen);
+                ConfigManager.ADVANCEMENT_TAB_SORTING_MODE.get().sortAdvancementTabs((Map<Advancement, Object>) obj);
+            }
+            catch(NoSuchFieldException | IllegalAccessException e) {
+                CrashHandler.getInstance().handleException("Failed to sort advancement tabs of BetterAdvancementsScreen!", "AdvancementTabsSorter", e, Level.ERROR);
+            }
+        }
     }
 
     @SubscribeEvent
     public static void beforeScreenInit(ScreenEvent.Init.Pre event){
-        if (event.getScreen() instanceof AdvancementsScreen) {
+        Screen screen = event.getScreen();
+        if (screen instanceof AdvancementsScreen || (BA_COMPATIBILITY.get() && screen instanceof BetterAdvancementsScreen)) {
             if(ConfigManager.NO_ADVANCEMENTS.get()){
                 Minecraft.getInstance().setScreen(null);
             }
