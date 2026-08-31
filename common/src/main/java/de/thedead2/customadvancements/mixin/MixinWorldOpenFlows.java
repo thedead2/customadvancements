b19f5d973dc1b095be5150f189c6092bd72ba85b
thedead2 <jannik.steins@gmx.net>
2026-08-31 16:10:19 +0200

    Fixed some fabric related issues to ensure mixin and accessWidener are correctly loaded

--- common/src/main/java/de/thedead2/customadvancements/mixin/MixinWorldOpenFlows.java
+++ common/src/main/java/de/thedead2/customadvancements/mixin/MixinWorldOpenFlows.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldOpenFlows.class)
 public class MixinWorldOpenFlows {
 
-    @Inject(at = @At("HEAD"), method = "openWorldLoadLevelStem(Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;Lcom/mojang/serialization/Dynamic;ZLjava/lang/Runnable;)V", remap = false)
+    @Inject(at = @At("HEAD"), method = "openWorldLoadLevelStem(Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;Lcom/mojang/serialization/Dynamic;ZLjava/lang/Runnable;)V")
     public void onLevelLoad(LevelStorageSource.LevelStorageAccess levelStorage, Dynamic<?> levelData, boolean safeMode, Runnable onFail, CallbackInfo ci) {
         CAMain.getInstance().getCustomAdvancementManager().setSaveMode(safeMode);
     }
